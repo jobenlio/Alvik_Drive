25e084004b74d9eaf900df08922ea977940a0c73
jobenlio <joben@gmx.ch>
2026-01-12 11:08:39 +0100

    added Arduino files with code

---
+++ RouteSave/RouteSave.ino
@@ -0,0 +1,52 @@
+#include "Arduino_Alvik.h"
+static Arduino_Alvik alvik;
+
+void randomDrive();
+void randomDrive();
+
+float sValues[10][9];
+
+
+
+void setup(){
+  Serial.begin(115200);
+  alvik.begin();
+}
+void loop(){
+  //alvik.get_orientation(sValues[0], sValues[1], sValues[2]); // Nur Yaw-Value (i=2) wird verwendet werden (ist die Rotation vom Start auf der Z-Achse)
+  //randomDrive();
+  //savedrive();
+
+  alvik.get_imu(sValues[1], sValues[2], sValues[3], sValues[4], sValues[5], sValues[6]);
+  Serial.println(sValues[1]);
+  Serial.println(sValues[2]);
+  Serial.println(sValues[3]);
+  Serial.println(sValues[4]);
+  Serial.println(sValues[5]);
+  Serial.println(sValues[6]);
+
+  // @Todo: Erkennung widerholte bewegung (wegen feststecken) bei der hinfahrt und auskorigieren bei rückfahrt.
+}
+
+void randomDrive(){
+  char buff [32];
+  int rand_maxValue = 13;
+  int rand_minValue = 5;
+  srand(time(NULL));
+
+  float testLinVel = rand() % (rand_maxValue - rand_minValue + 1) + rand_minValue;
+  float testAngVel = rand() % (25 + 1) + rand_minValue;
+
+  /*sprintf(buff,"LinVel:%f  AngVel:%f", testLinVel, testAngVel);
+  Serial.println(buff);*/
+  
+  alvik.drive(testLinVel, testAngVel);
+}
+
+void savedrive(){
+  static char i = 0;
+  alvik.get_imu(sValues[i][1], sValues[i][2], sValues[i][3], sValues[i][4], sValues[i][5], sValues[i][6]);
+  i++;
+}
+
+
---
+++ RouteSave/sketch_jan7a/sketch_jan7a.ino
@@ -0,0 +1,43 @@
+#include "Arduino_Alvik.h"
+
+static Arduino_Alvik alvik;
+
+float sValues[10][6];
+
+void setup(){
+  Serial.begin(115200);
+  alvik.begin();
+}
+
+void savedrive();
+
+void loop(){
+  //alvik.get_orientation(sValues[0], sValues[1], sValues[2]); // Nur Yaw-Value (i=2) wird verwendet werden (ist die Rotation vom Start auf der Z-Achse)
+  if (alvik.get_touch_any()){
+    if(alvik.get_touch_up()){
+      Serial.println("up");
+      alvik.drive(10, 27);
+      savedrive();
+    }
+
+    else if(alvik.get_touch_down()){
+      Serial.println("down");
+      for(int index=0; index < 10; index++){
+        for(int i=0; i<6;i++){
+          Serial.println(sValues[index][i]);
+        }
+      }
+    }
+  }
+  
+}
+
+void savedrive(){
+  static int aIndexSave = 0;
+  for(aIndexSave; aIndexSave<5; aIndexSave++){
+    alvik.get_imu(sValues[aIndexSave][1], sValues[aIndexSave][2], sValues[aIndexSave][3], sValues[aIndexSave][4], sValues[aIndexSave][5], sValues[aIndexSave][6]);
+    Serial.println("Drive saved");
+  }
+}
+
+
