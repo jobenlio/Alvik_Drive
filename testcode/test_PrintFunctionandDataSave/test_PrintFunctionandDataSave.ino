8370a4b51de32ea3db9ce44a79c7fa55347011f4
jobenlio <joben@gmx.ch>
2026-01-15 18:41:43 +0100

    tested function get_drive_speed
    is the correct values but it ignores if driving into a wall.

--- testcode/test_PrintFunctionandDataSave/test_PrintFunctionandDataSave.ino
+++ testcode/test_PrintFunctionandDataSave/test_PrintFunctionandDataSave.ino
@@ -8,22 +8,14 @@ void setup() {
 }
 
 void loop() {
-  alvik.drive(2, 0);
-  float sValues[6][2];
+  alvik.drive(3, -40);
+  float sValues[2][2];
 
-  alvik.get_imu(sValues[0][0], sValues[0][1], sValues[0][2], sValues[0][3], sValues[0][4], sValues[0][5]);
+  alvik.get_drive_speed(sValues[0][0], sValues[0][1]);
 
   Serial.print(sValues[0][0]);
   Serial.print("\t");
-  Serial.print(sValues[0][1]);
-  Serial.print("\t");
-  Serial.print(sValues[0][2]);
-  Serial.print("\t");
-  Serial.print(sValues[0][3]);
-  Serial.print("\t");
-  Serial.print(sValues[0][4]);
-  Serial.print("\t");
-  Serial.println(sValues[0][5]);
+  Serial.println(sValues[0][1]);
 
   delay(100);
 }
