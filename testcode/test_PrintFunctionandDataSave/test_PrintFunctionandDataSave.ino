13caa3453b12a6e72408ca59c82e3483acaebca8
jobenlio <joben@gmx.ch>
2026-01-15 18:48:12 +0100

    tested saving values with index
    intoduces index "i" to save values in different spaces, instead of deleting value before

--- testcode/test_PrintFunctionandDataSave/test_PrintFunctionandDataSave.ino
+++ testcode/test_PrintFunctionandDataSave/test_PrintFunctionandDataSave.ino
@@ -9,13 +9,14 @@ void setup() {
 
 void loop() {
   alvik.drive(3, -40);
-  float sValues[2][2];
+  float sValues[50][2];
+  static int i = 0;
+  i>49? i = 0: i++;
+  alvik.get_drive_speed(sValues[i][0], sValues[i][1]);
 
-  alvik.get_drive_speed(sValues[0][0], sValues[0][1]);
-
-  Serial.print(sValues[0][0]);
+  Serial.print(sValues[i][0]);
   Serial.print("\t");
-  Serial.println(sValues[0][1]);
+  Serial.println(sValues[i][1]);
 
   delay(100);
 }
