3017d1068f3ac9e8b0008543f22a5e284db6c624
jobenlio <joben@gmx.ch>
2026-01-15 18:31:59 +0100

    print action changed
    printing with one buffer restartet whole programm every time. even so the imu function is wrong. instead probably the "get_drive_speed" function should be good

--- testcode/test_PrintFunctionandDataSave/test_PrintFunctionandDataSave.ino
+++ testcode/test_PrintFunctionandDataSave/test_PrintFunctionandDataSave.ino
@@ -8,13 +8,22 @@ void setup() {
 }
 
 void loop() {
+  alvik.drive(2, 0);
   float sValues[6][2];
 
   alvik.get_imu(sValues[0][0], sValues[0][1], sValues[0][2], sValues[0][3], sValues[0][4], sValues[0][5]);
 
-  char buff [50];
-  sprintf(buff, "%f\t%f\t%f\t%f\t%f\t%f", sValues[0][0], sValues[0][1], sValues[0][2], sValues[0][3], sValues[0][4], sValues[0][5]);
-  Serial.println(buff);
+  Serial.print(sValues[0][0]);
+  Serial.print("\t");
+  Serial.print(sValues[0][1]);
+  Serial.print("\t");
+  Serial.print(sValues[0][2]);
+  Serial.print("\t");
+  Serial.print(sValues[0][3]);
+  Serial.print("\t");
+  Serial.print(sValues[0][4]);
+  Serial.print("\t");
+  Serial.println(sValues[0][5]);
 
-  delay(10);
+  delay(100);
 }
