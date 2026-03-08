72842b864c90811b387952ba53f1c0910a8b6e8f
jobenlio <joben@gmx.ch>
2026-03-08 16:53:47 +0100

    faulty code
    faulty code but changes made that have functioned bevor

--- RouteSave/RouteSave.ino
+++ RouteSave/RouteSave.ino
@@ -12,16 +12,29 @@ void setup(){
 }
 
 void loop(){
-  alvik.drive(12, 20);
-  savedrive();
-  Serial.println("Drive saved");
+  static char start = 1;
+  if(start = 1)
+  {
+    start = 0;
+    for (int i = 0; i<100; i++)
+    {
+      alvik.drive(12, 20);
+      savedrive();
+      Serial.println("Drive saved");
+    }
+    for(int i = 0; i<100; i++)
+    {
+      alvik.drive(sValues[i][0], sValues[i][1]);
+    }
+  }
+
 }
 
 void savedrive(){
   static char sSaveIndex = 0;
   alvik.get_drive_speed(sValues[sSaveIndex][0], sValues[sSaveIndex][1]);
-
-  sValues[sSaveIndex][1] = -1 * sValues[sSaveIndex][1];
+  sValues[sSaveIndex][0] = sValues[sSaveIndex][0] / 10;   // correcting fals get of 10x more than in drive given value
+  sValues[sSaveIndex][1] = -1 * sValues[sSaveIndex][1];   // changing direction 
   sSaveIndex++;
 }
 
--- testcode/test_DringingSavedValues/test_DringingSavedValues.ino
+++ testcode/test_DringingSavedValues/test_DringingSavedValues.ino
@@ -5,6 +5,8 @@
 */
 
 #include "Arduino_Alvik.h"
+#include <stdlib.h>
+
 static Arduino_Alvik alvik;
 
 void savedrive(); // Function to save the driving values
@@ -17,28 +19,43 @@ float sValues[100][2];  // Array where the drive values are stored
 void setup(){
   Serial.begin(115200);
   alvik.begin();
+  Serial.println("Beginning");
 }
 
 /**
 * @brief Super loop function.
 */
 void loop(){
+
+  Serial.print("WHY????");
+static char startBool = 1;
+  if(startBool == 1){
+    startBool = 0;
+    for (int in= 0;in<100;in++ ){
+    alvik.drive(5, 45);   // Alvik drives in a Circle with an angle of 45 deg/s
+    savedrive();          // Saving the current values of the lin. and angular velocity
+    delay(100);
+    }
+  }
+
   if(alvik.get_touch_any()){  // Looking if any buton on the alvik got pressed
 
     /*Up button: driving a defined way for 10 s*/
-    if(alvik.get_touch_up()){
-      for (int in= 0;in<100;in++ ){
-        alvik.drive(5, 45);   // Alvik drives in a Circle with an angle of 45 deg/s 
-        savedrive();          // Saving the current values of the lin. and angular velocity
+
+
+    /*If the down button got pressed the driving form before gets driven agan*/
+    if(alvik.get_touch_down()){ 
+      for(int i = 0; i<100;i++){
+        alvik.drive(sValues[i][0], sValues[i][1]);
         delay(100);
       }
     }
 
-    /*If the down button got pressed the driving form before gets driven agan*/
-    else if(alvik.get_touch_down()){ 
+    else if (alvik.get_touch_center()){
       for(int i = 0; i<100;i++){
-        alvik.drive(sValues[i][0], sValues[i][1]);
-        delay(100);
+          Serial.print(sValues[i][0]);
+          Serial.print("\t");
+          Serial.println(sValues[i][1]);
       }
     }
   }
@@ -49,5 +66,7 @@ void savedrive(){
   // Initialising Index 
   static int sSaveIndex = 0;
   alvik.get_drive_speed(sValues[sSaveIndex][0], sValues[sSaveIndex][1]); // Saving the linear- and angular velocity of the robot
+  sValues[sSaveIndex][0] = sValues[sSaveIndex][0] / 10;   // correcting fals get of 10x more than in drive given value
+  sValues[sSaveIndex][1] = -1 * sValues[sSaveIndex][1];   // changing direction 
   sSaveIndex > 99? sSaveIndex = 0: sSaveIndex++;  // Restarting Index since for this test the array is just 100 long
 }
